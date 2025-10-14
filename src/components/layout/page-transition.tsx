4453ddd9534ab115a865dfeab2494c59ffd9710b
Praveen J <praveenjayakumar2006@gmail.com>
2025-10-14 17:13:44 +0000

    when i move from home to booking page, i could see a samll animation lik

--- src/components/layout/page-transition.tsx
+++ src/components/layout/page-transition.tsx
@@ -10,9 +10,9 @@ export function PageTransition({ children }: { children: React.ReactNode }) {
     <AnimatePresence mode="wait">
       <motion.div
         key={pathname}
-        initial={{ opacity: 0, y: 10 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -10 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
         transition={{ duration: 0.2 }}
         className="flex flex-col flex-1"
       >
