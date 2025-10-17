93c835caedaab062661f59a00b17707c75573a9a
Praveen J <praveenjayakumar2006@gmail.com>
2025-10-17 18:13:23 +0000

    My Bookings add a small text below this

--- src/app/reservations/page.tsx
+++ src/app/reservations/page.tsx
@@ -4,7 +4,10 @@ import { ReservationsTable } from '@/components/reservations/reservations-table'
 export default function ViewBookingsPage() {
   return (
     <div className="mx-auto grid w-full max-w-6xl gap-2">
-       <h1 className="text-3xl font-semibold">My Bookings</h1>
+       <div>
+        <h1 className="text-3xl font-semibold">My Bookings</h1>
+        <p className="text-muted-foreground">View and manage your past, active, and upcoming reservations.</p>
+       </div>
        <div className="mt-4">
         <ReservationsTable />
       </div>
