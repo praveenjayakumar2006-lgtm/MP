1d5255c97728103a913f7219337107360232ec54
Praveen J <praveenjayakumar2006@gmail.com>
2025-10-17 17:08:27 +0000

    include this icon in log in and sign up (_for element <LinkComponent>_)

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,4 +1,6 @@
 
+import { Car } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react';
 
 export default function AuthLayout({
@@ -7,7 +9,14 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-1 items-center justify-center bg-background">
+    <div className="flex flex-1 flex-col items-center justify-center bg-background gap-6">
+       <Link
+          href="/home"
+          className="flex items-center gap-2 text-lg font-semibold"
+        >
+          <Car className="h-10 w-10 text-primary" />
+          <span className="font-bold text-2xl">ParkEasy</span>
+        </Link>
       {children}
     </div>
   );
