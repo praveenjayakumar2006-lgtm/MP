4ee0771486c1c4c5fe4ad0b205589484a2fe3651
Praveen J <praveenjayakumar2006@gmail.com>
2025-10-25 04:34:49 +0000

    make the pop up reservation successful to the bottom and also in small s

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -16,7 +16,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed bottom-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:flex-col md:max-w-[420px] sm:right-0",
+      "fixed bottom-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 md:max-w-[420px]",
       className
     )}
     {...props}
