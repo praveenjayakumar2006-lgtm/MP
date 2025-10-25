7cc17d7f84fbd12500af9de271190d8bcadacf32
Praveen J <praveenjayakumar2006@gmail.com>
2025-10-25 15:23:22 +0000

    make the animation of date for time and duration. when curson goes near

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -219,7 +219,7 @@ export default function BookingPage() {
                                 disabled={!selectedDate}
                               >
                                   <FormControl>
-                                  <SelectTrigger className="disabled:cursor-not-allowed disabled:opacity-50">
+                                  <SelectTrigger className="disabled:cursor-not-allowed disabled:opacity-50 hover:bg-background">
                                     <div className="flex items-center gap-2">
                                       <Clock className="h-4 w-4" />
                                       <SelectValue placeholder="Select a time" />
@@ -250,7 +250,7 @@ export default function BookingPage() {
                               <FormLabel>Duration (Hours)</FormLabel>
                               <Select onValueChange={field.onChange} value={field.value} disabled={!selectedStartTime}>
                                   <FormControl>
-                                  <SelectTrigger className="disabled:cursor-not-allowed disabled:opacity-50">
+                                  <SelectTrigger className="disabled:cursor-not-allowed disabled:opacity-50 hover:bg-background">
                                      <div className="flex items-center gap-2">
                                         <Hourglass className="h-4 w-4" />
                                         <SelectValue placeholder="Select hours" />
