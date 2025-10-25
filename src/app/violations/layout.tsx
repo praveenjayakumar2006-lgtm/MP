6c38ae408ae3e951960c0ee1a3be5d84096b455e
Praveen J <praveenjayakumar2006@gmail.com>
2025-10-25 16:11:33 +0000

    still scrolable. i want in single page. no scrollabe;

--- src/app/violations/layout.tsx
+++ src/app/violations/layout.tsx
@@ -8,7 +8,7 @@ export default function ViolationsLayout({
 }) {
   return (
     <div className="flex flex-1 flex-col bg-background">
-      <div className="container mx-auto px-4 md:px-6 flex-1 flex py-8">
+      <div className="container mx-auto px-4 md:px-6 flex-1 flex">
         {children}
       </div>
     </div>
