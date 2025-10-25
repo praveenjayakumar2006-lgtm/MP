0085af365779acc97025fa399907a95febd865bc
Praveen J <praveenjayakumar2006@gmail.com>
2025-10-25 15:58:21 +0000

    All Active Upcoming Completed Slot ID	Vehicle Plate	Start Time	End Time

--- src/app/reservations/layout.tsx
+++ src/app/reservations/layout.tsx
@@ -8,7 +8,7 @@ export default function ReservationsLayout({
 }) {
   return (
     <div className="flex flex-1 flex-col bg-background">
-      <div className="container mx-auto py-8 px-4 md:px-6 flex-1">
+      <div className="container mx-auto py-8 px-4 md:px-6 flex-1 w-full">
         {children}
       </div>
     </div>
