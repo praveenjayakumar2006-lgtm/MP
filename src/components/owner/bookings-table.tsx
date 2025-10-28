55609ae576bb7e14af53e95395182240f3f8724f
Praveen J <praveenjayakumar2006@gmail.com>
2025-10-28 08:33:25 +0000

    remove the status option, i mean remove that column. All Active Upcoming

--- src/components/owner/bookings-table.tsx
+++ src/components/owner/bookings-table.tsx
@@ -167,7 +167,6 @@ export function BookingsTable() {
         <TableCell><Skeleton className="h-5 w-24" /></TableCell>
         <TableCell><Skeleton className="h-5 w-40" /></TableCell>
         <TableCell><Skeleton className="h-5 w-40" /></TableCell>
-        <TableCell><Skeleton className="h-5 w-16" /></TableCell>
       </TableRow>
     ));
 
@@ -197,7 +196,6 @@ export function BookingsTable() {
                     <TableHead>Vehicle Plate</TableHead>
                     <TableHead>Start Time</TableHead>
                     <TableHead>End Time</TableHead>
-                    <TableHead>Status</TableHead>
                   </TableRow>
                 </TableHeader>
                 <TableBody>
@@ -226,9 +224,6 @@ export function BookingsTable() {
                       <TableCell>{reservation.vehiclePlate}</TableCell>
                       <TableCell>{format(new Date(reservation.startTime), getDateFormat())}</TableCell>
                       <TableCell>{format(new Date(reservation.endTime), getDateFormat())}</TableCell>
-                      <TableCell>
-                        <StatusIcon status={reservation.status} />
-                      </TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
